cf2e8edd83f63b5ff23581eea37a39f73c1b9c07
beingEngg <shaikhasifmech@gmail.com>
2023-03-27 23:46:35 +0530

    starting commit

---
+++ Room.java
@@ -0,0 +1,24 @@
+class Room{
+   public static void main(String[] args){
+    Example e1=new Example();
+    Example e2=new Example();
+
+   }
+}
+Tomcat installation s/w link
+https://tomcat.apache.org/download-10.cgi
+
+cycle
+https://www.amazon.in/Firefox-Bikes-Bad-Attitude-Mountain/dp/B098Q8DR11/ref=sr_1_4?crid=3TFMD2VK3O58R&keywords=bad+attitude+10&qid=1652641688&sprefix=bad+a%2Caps%2C471&sr=8-4
+power bank
+https://www.amazon.in/20000mAh-Sandstone-Triple-Charging-Delivery/dp/B08HV83HL3/ref=sr_1_1?crid=1DCDQYC9JSG4D&keywords=mi%2Bpower%2Bbank%2Bfast%2Bcharging&qid=1652646022&sprefix=mi%2Bpower%2Bbank%2Bfas%2Caps%2C494&sr=8-1&th=1
+bluetooth speaker
+https://www.flipkart.com/jbl-flip-essential-ipx7-waterproof-16-w-bluetooth-speaker/p/itme99859d0d92a2?pid=ACCFMPZGUUFHJCZF&lid=LSTACCFMPZGUUFHJCZF2G9PSD&marketplace=FLIPKART&q=jbl+speaker&store=0pm%2F0o7&spotlightTagId=BestsellerId_0pm%2F0o7&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=abc44ceb-2cba-48d9-9671-415dda41dc35.ACCFMPZGUUFHJCZF.SEARCH&ppt=sp&ppn=sp&ssid=44hzu3so1c0000001652648382317&qH=1b34d1fbc41c9466
+mouse+keyboard
+https://www.amazon.in/Logitech-MK295-Wireless-Keyboard-Mouse/dp/B08FMLQXN4/ref=sr_1_20?crid=8NF96URIF6KW&keywords=wireless%2Bmouse%2Bkeyboard%2Bset&qid=1652650246&s=electronics&sprefix=wireless%2Bmo%2Celectronics%2C442&sr=1-20&th=1
+
+
+1st dec 2022
+job posting for 2-5yrs
+mail id:ravi201606@gmail.com
+JD(Java,JEE,Spring stack,JSON,JSP,Mongo Db,PostGRESQL,Elastic Search,GIT,Docker(knowledge),Linux)
