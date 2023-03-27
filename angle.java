ab14eedfa8b4ee61242f03f88e75c97698aa87e3
beingEngg <shaikhasifmech@gmail.com>
2023-03-28 00:30:13 +0530

    bug fix 130

--- angle.java
+++ angle.java
@@ -3,6 +3,8 @@ int angle1=60;
 int angle2=60;
 int angle3=60;
 int sumOfAngles;
+//bug-130
+int rightAngle=90;
 public static void main(String []args){
 sumOfAngles=angle1+angle2+angle3;
 System.out.println("Total angle"+sumOfAngles);
