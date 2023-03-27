15af6c61512cbd249d81220cb65bbc3e87b0e17e
beingEngg <128593584+beingEngg@users.noreply.github.com>
2023-03-28 00:02:45 +0530

    Update angle.java

---
+++ angle.java
@@ -0,0 +1,11 @@
+class FindAngle{
+int angle1=60;
+int angle2=60;
+int angle3=60;
+int sumOfAngles;
+public static void main(String []args){
+sumOfAngles=angle1+angle2+angle3;
+System.out.println("Total angle"+sumOfAngles);
+}
+
+}
